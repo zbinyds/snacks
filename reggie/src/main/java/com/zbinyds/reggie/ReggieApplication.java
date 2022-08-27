c305866f82e68967b8a60454dd390a8b5e6e9a0f
ZhangBin <ZhangBin@qq.com>
2022-08-27 12:42:16 +0800

    Spring Cache整合Redis缓存套餐数据

--- reggie/src/main/java/com/zbinyds/reggie/ReggieApplication.java
+++ reggie/src/main/java/com/zbinyds/reggie/ReggieApplication.java
@@ -4,11 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @MapperScan("com.zbinyds.reggie.mapper")
 @EnableTransactionManagement // 启用事务管理器
+@EnableCaching // 启用spring缓存机制，这里使用的是redis管理缓存。
 public class ReggieApplication {
 
     public static void main(String[] args) {
--- reggie/src/main/java/com/zbinyds/reggie/service/impl/SetmealServiceImpl.java
+++ reggie/src/main/java/com/zbinyds/reggie/service/impl/SetmealServiceImpl.java
@@ -17,6 +17,9 @@ import com.zbinyds.reggie.service.SetmealService;
 import com.zbinyds.reggie.mapper.SetmealMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -31,7 +34,7 @@ import java.util.concurrent.TimeUnit;
  *
  */
 @Service
-public class SetmealServiceImpl extends ServiceImpl<SetmealMapper, Setmeal> implements SetmealService{
+public class SetmealServiceImpl extends ServiceImpl<SetmealMapper, Setmeal> implements SetmealService {
 
     @Autowired
     private SetmealDishService setmealDishService;
@@ -42,10 +45,11 @@ public class SetmealServiceImpl extends ServiceImpl<SetmealMapper, Setmeal> impl
     @Autowired(required = false)
     private StringRedisTemplate stringRedisTemplate;
 
-    @Autowired(required = false)
-    private ObjectMapper objectMapper;
-
+    /**
+     * 使用Spring cache整合Redis管理套餐缓存
+     */
     @Transactional // 涉及到多张表操作，需要开启事务
+    @CacheEvict(value = "setmealCache", key = "#setmealDto.categoryId")
     public void saveSetmealAndDish(SetmealDto setmealDto) {
         // 首先向套餐表添加数据
         this.save(setmealDto);
@@ -55,10 +59,6 @@ public class SetmealServiceImpl extends ServiceImpl<SetmealMapper, Setmeal> impl
             setmealDish.setSetmealId(String.valueOf(setmealDto.getId()));
         }
         setmealDishService.saveBatch(setmealDishes);
-
-        // 删除该套餐分类的缓存信息
-        Set<String> keys = stringRedisTemplate.keys("setMeal_" + setmealDto.getCategoryId());
-        stringRedisTemplate.delete(keys);
     }
 
     @Override
@@ -74,17 +74,21 @@ public class SetmealServiceImpl extends ServiceImpl<SetmealMapper, Setmeal> impl
 
         // 再查询套餐中的菜品信息
         QueryWrapper<SetmealDish> setmealDishQueryWrapper = new QueryWrapper<>();
-        setmealDishQueryWrapper.eq("setmeal_id",setMealId);
+        setmealDishQueryWrapper.eq("setmeal_id", setMealId);
         List<SetmealDish> list = setmealDishService.list(setmealDishQueryWrapper);
 
         // 将查询得到的结果全部封装到 setmealDto 对象中，方便进行数据传输。
         SetmealDto setmealDto = new SetmealDto();
-        BeanUtils.copyProperties(setmeal,setmealDto); // 将setmeal对象复制并赋值给setmealDto对象
+        BeanUtils.copyProperties(setmeal, setmealDto); // 将setmeal对象复制并赋值给setmealDto对象
         setmealDto.setSetmealDishes(list);
         return setmealDto;
     }
 
+    /**
+     * 使用Spring cache整合Redis管理套餐缓存
+     */
     @Transactional
+    @CacheEvict(value = "setmealCache", key = "#setmealDto.categoryId")
     public void updateSetMealAndDish(SetmealDto setmealDto) {
         // 首先修改套餐基本信息
         this.updateById(setmealDto);
@@ -92,7 +96,7 @@ public class SetmealServiceImpl extends ServiceImpl<SetmealMapper, Setmeal> impl
         // 修改套餐中的菜品信息（先清空再添加）
         // 1、先清空改套餐中的菜品信息
         QueryWrapper<SetmealDish> setmealDishQueryWrapper = new QueryWrapper<>();
-        setmealDishQueryWrapper.eq("setmeal_id",setmealDto.getId());
+        setmealDishQueryWrapper.eq("setmeal_id", setmealDto.getId());
         setmealDishService.remove(setmealDishQueryWrapper);
 
         // 2、再重新添加修改后的菜品信息
@@ -102,79 +106,45 @@ public class SetmealServiceImpl extends ServiceImpl<SetmealMapper, Setmeal> impl
             setmealDish.setSetmealId(String.valueOf(setmealDto.getId()));
         }
         setmealDishService.saveBatch(setmealDishes);
-
-        // 删除该套餐分类的缓存信息
-        Set<String> keys = stringRedisTemplate.keys("setMeal_" + setmealDto.getCategoryId());
-        stringRedisTemplate.delete(keys);
     }
 
+    /**
+     * 注：因为修改套餐状态会清除缓存，而删除菜品前必须修改套餐状态为停售，所以这里没必要在清理缓存了。
+     */
     @Transactional
     public void removeSetmealAndDish(String ids) {
         // 将套餐id按逗号进行分割，获取id数组
         String[] idArr = ids.split(",");
         // 将id数组转成Long类型的list集合
         ArrayList<Long> idList = new ArrayList<>(idArr.length);
-        HashSet<Long> set = new HashSet<>(); // 所删除套餐对应的分类id，用于删除分类缓存信息。
         for (String id : idArr) {
             idList.add(Long.valueOf(id));
-
-            // 查询每个套餐对应的分类id，并将其添加到set集合中
-            LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
-            queryWrapper.eq(Setmeal::getId, id);
-            Setmeal setmeal = this.getOne(queryWrapper);
-            set.add(setmeal.getCategoryId());
         }
 
         // 1、判断套餐是否能被删除，如果套餐正在启售状态，则不能删除。
         QueryWrapper<Setmeal> setmealQueryWrapper = new QueryWrapper<>();
-        setmealQueryWrapper.in("id",idList).eq("status",1);
+        setmealQueryWrapper.in("id", idList).eq("status", 1);
         long count = this.count(setmealQueryWrapper);
-        if (count > 0){ // 说明传递过来的套餐中，有套餐正在启售中，此时不能删除
+        if (count > 0) { // 说明传递过来的套餐中，有套餐正在启售中，此时不能删除
             throw new CustomException("含有正在启售的套餐，不能删除！");
         }
         // 2、删除套餐表中对应的数据
         this.removeBatchByIds(idList);
         // 3、删除套餐菜品表对应的数据
         QueryWrapper<SetmealDish> setmealDishQueryWrapper = new QueryWrapper<>();
-        setmealDishQueryWrapper.in("setmeal_id",idList);
+        setmealDishQueryWrapper.in("setmeal_id", idList);
         setmealDishService.remove(setmealDishQueryWrapper);
-
-        // 清理所删除套餐对应的分类缓存，如果存在多个套餐，则将它们对应的分类缓存全部清空。
-        for (Long aLong : set) {
-            Set<String> keys = stringRedisTemplate.keys("setMeal_" + aLong);
-            stringRedisTemplate.delete(keys);
-        }
     }
 
-    @Override
+    /**
+     * 使用Spring cache整合Redis管理套餐缓存
+     */
+    @Cacheable(value = "setmealCache", key = "#setmeal.categoryId")
     public List<Setmeal> getSetMealList(Setmeal setmeal) throws JsonProcessingException {
-        List<Setmeal> list = null;
-
-        /*
-         * 1、先尝试从缓存中获取数据
-         * */
-        String key = "setMeal_" + setmeal.getCategoryId();
-        String s = stringRedisTemplate.opsForValue().get(key);
-
-        /*
-         * 2、如果缓存中存在数据，直接返回，无需查询数据库
-         * */
-        if (s != null) {
-            // 将json字符串转成java对象
-            list = objectMapper.readValue(s, List.class);
-            return list;
-        }
-
-        /*
-         * 3、缓存中不存在数据，需要查询数据库，并且将查询结果缓存到Redis中
-         * */
         QueryWrapper<Setmeal> setmealQueryWrapper = new QueryWrapper<>();
         setmealQueryWrapper.eq(setmeal.getCategoryId() != null, "category_id", setmeal.getCategoryId());
         setmealQueryWrapper.eq(setmeal.getStatus() != null, "status", setmeal.getStatus());
-        list = this.list(setmealQueryWrapper);
-
-        // 将该分类下的套餐信息缓存到Redis中，有效期为60分钟。
-        stringRedisTemplate.opsForValue().set(key, objectMapper.writeValueAsString(list), 1, TimeUnit.HOURS);
+        List<Setmeal> list = this.list(setmealQueryWrapper);
         return list;
     }
 
@@ -195,10 +165,9 @@ public class SetmealServiceImpl extends ServiceImpl<SetmealMapper, Setmeal> impl
         }
         // 清理所修改套餐对应的分类缓存，如果存在多个套餐，则将它们对应的分类缓存全部清空。
         for (Long aLong : set) {
-            Set<String> keys = stringRedisTemplate.keys("setMeal_" + aLong);
+            Set<String> keys = stringRedisTemplate.keys("setmealCache::" + aLong);
             stringRedisTemplate.delete(keys);
         }
-
         this.updateBatchById(setmeals);
     }
 
@@ -206,16 +175,16 @@ public class SetmealServiceImpl extends ServiceImpl<SetmealMapper, Setmeal> impl
     public SetmealDto getSetmealAndDish(String id) {
         // 获取该套餐的基本信息
         LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(Setmeal::getId,id);
+        queryWrapper.eq(Setmeal::getId, id);
         Setmeal setmeal = this.getOne(queryWrapper);
 
         // 将setmeal对象copy给setmealDto对象，方便数据传输
         SetmealDto setmealDto = new SetmealDto();
-        BeanUtils.copyProperties(setmeal,setmealDto);
+        BeanUtils.copyProperties(setmeal, setmealDto);
 
         // 查询该套餐中的菜品数据，并将其设置给setmealDto对象
         LambdaQueryWrapper<SetmealDish> lambdaQueryWrapper = new LambdaQueryWrapper<>();
-        lambdaQueryWrapper.eq(SetmealDish::getSetmealId,id);
+        lambdaQueryWrapper.eq(SetmealDish::getSetmealId, id);
         List<SetmealDish> list = setmealDishService.list(lambdaQueryWrapper);
         setmealDto.setSetmealDishes(list);
 
